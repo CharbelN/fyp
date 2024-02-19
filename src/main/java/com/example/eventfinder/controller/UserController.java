6a20bd397478437f505663d5ed9ba56301a31f0a
Charbel Nassar <CharbelNassar@itb-me.com>
2024-02-19 10:08:39 +0200

    small fix

--- src/main/java/com/example/eventfinder/controller/UserController.java
+++ src/main/java/com/example/eventfinder/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.eventfinder.controller;
 
-import com.example.eventfinder.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
